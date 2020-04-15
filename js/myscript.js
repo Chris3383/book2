196bb6c44ee1fd886107df5d8d6b95a6d02434a5
Chris3383 <39918027+Chris3383@users.noreply.github.com>
2020-04-15 19:35:20 -0400

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -7,6 +7,7 @@ function loadBooks() {
   for (x = 0; x < booksObj.books.length; x++)
    {
     showBooks(booksObj, x);
+    showBooksInfo(booksObj, x)
    }
   }
  };
@@ -24,25 +25,10 @@ function showBooks(booksObj, x){
   document.getElementById(coverID).src = book.coverImage;
 }
 
-function loadAuthor(){
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-  if (this.readystate == 4 && this.status == 200) {
-   var authors = this.responseText;
-   var authorsObj = JSON.parse(authors);
-   for (x = 0; x < authorsObj.authors.length; x++)
-    {
-     showBooks(authorsObj, x);
-    }
-   }
-  };
-  xhttp.open("GET", "books.json", true);
-  xhttp.send();
- }
-
- function showAuthors(authorsObj, x){
-   var author = authorsObj.authors[x];
-   var authorID = "book" + x + "Author";
-
-   document.getElementById(titleID).innerHTML = book.title;
- }
+function showBooksInfo(booksObj, x){
+  var book = booksObj.books[x];
+  var authorID = "book" + x + "Author";
+  var reviewsID = "book" + x + "Reviews";
+  document.getElementById(authorID).innerHTML = book.author;
+  document.getElementById(reviewID).innerHTML = book.reviews;
+}
